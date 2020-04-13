ef3a77b44d69ce2fff8acc324304d514e24a1270
Yasuhiro Usutani <y_usutani@mac.com>
2020-04-13 10:50:16 +0900

    Book.create!(title: Faker::Book.title, row_order: i)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do |i|
+  Book.create!(title: Faker::Book.title, row_order: i)
+end
